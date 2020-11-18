7d51c9056a3c2ccc3d2ae28cfaf759794f1da9cc
rohith-webapp-iiht <74677478+rohith-webapp-iiht@users.noreply.github.com>
2020-11-19 00:08:04 +0530

    Update StockPricingApplication.java

--- stock-pricing/src/main/java/com/prabhu/stockpricing/StockPricingApplication.java
+++ stock-pricing/src/main/java/com/prabhu/stockpricing/StockPricingApplication.java
@@ -1,5 +1,4 @@
 package com.prabhu.stockpricing;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
