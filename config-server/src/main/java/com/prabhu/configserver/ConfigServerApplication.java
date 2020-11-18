f79b39934b04189223b0089b3278adc22169b7dd
rohith-webapp-iiht <74677478+rohith-webapp-iiht@users.noreply.github.com>
2020-11-19 00:07:13 +0530

    Update ConfigServerApplication.java

--- config-server/src/main/java/com/prabhu/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/prabhu/configserver/ConfigServerApplication.java
@@ -1,5 +1,4 @@
 package com.prabhu.configserver;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
