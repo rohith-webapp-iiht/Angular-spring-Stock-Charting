551e1a02121aa2ebc24b884ef2236b5a45cf2d0f
Rohith R <44117122+rohithr8484@users.noreply.github.com>
2020-11-18 23:15:35 +0530

    Update GraphingServiceApplication.java

--- graphing-service/src/main/java/com/prabhu/graphingservice/GraphingServiceApplication.java
+++ graphing-service/src/main/java/com/prabhu/graphingservice/GraphingServiceApplication.java
@@ -1,5 +1,4 @@
 package com.prabhu.graphingservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
