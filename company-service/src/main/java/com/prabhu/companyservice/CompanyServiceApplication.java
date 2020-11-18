bdbe088510c745bd129afee6a384eac281b17015
rohith-webapp-iiht <74677478+rohith-webapp-iiht@users.noreply.github.com>
2020-11-19 00:06:46 +0530

    Update CompanyServiceApplication.java

--- company-service/src/main/java/com/prabhu/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/com/prabhu/companyservice/CompanyServiceApplication.java
@@ -1,5 +1,4 @@
 package com.prabhu.companyservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
