ecda2c800d7f2d7e61ac62da0eb948d2b6675bd3
rohith-webapp-iiht <74677478+rohith-webapp-iiht@users.noreply.github.com>
2020-11-19 00:13:00 +0530

    Update ZuulProxyApplication.java

--- zuul-proxy/src/main/java/com/prabhu/zuulproxy/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/com/prabhu/zuulproxy/ZuulProxyApplication.java
@@ -1,5 +1,4 @@
 package com.prabhu.zuulproxy;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
