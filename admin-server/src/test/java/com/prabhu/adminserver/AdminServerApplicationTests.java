ca382d000e7fda5195da05626f1914fd08bae6c4
rohith-webapp-iiht <74677478+rohith-webapp-iiht@users.noreply.github.com>
2020-11-19 12:46:36 +0530

    Update AdminServerApplicationTests.java

--- admin-server/src/test/java/com/prabhu/adminserver/AdminServerApplicationTests.java
+++ admin-server/src/test/java/com/prabhu/adminserver/AdminServerApplicationTests.java
@@ -2,7 +2,6 @@ package com.prabhu.adminserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class AdminServerApplicationTests {
 
